e24afbeaffa26a7ca77deba3f5696d64c5d3cc58
mohamud-eslan <mohmud141@gmail.com>
2022-11-19 14:24:51 +0300

    is number even or odd

---
+++ isEvenOrOdd.js
@@ -0,0 +1,10 @@
+function isEvenOrOdd(num){
+    if (num % 2 == 0) {
+        return 'even';
+    } else {
+        return 'odd';
+    }
+}
+console.log(isEvenOrOdd(8));
+console.log(isEvenOrOdd(21));
+console.log(isEvenOrOdd(5));
