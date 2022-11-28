b01aecd51f2f9db2ecc8c8eefd35f12a5d88a66e
mohamud-eslan <mohmud141@gmail.com>
2022-11-28 21:14:10 +0300

    an other commit

--- callback.js
+++ callback.js
@@ -1,14 +1,30 @@
-function callback(data){
-    if(data != undefined){
-        console.log("added with five", data + 5);
-    }else{
-        console.log("please prodive the first pramater");
-    }
-}
-function addFive(data, callback){
-    if(data != undefined){
-        console.log("calling function", data);
-    }
-    callback(data)
-}
-addFive(50, callback)
+// function callback(data){
+//     if(data != undefined){
+//         console.log("added with five", data + 5);
+//     }else{
+//         console.log("please prodive the first pramater");
+//     }
+// }
+// function addFive(data, callback){
+//     if(data != undefined){
+//         console.log("calling function", data);
+//     }
+//     callback(data)
+// }
+// addFive(50, callback)
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
+  
+  async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+  }
+  
+  asyncCall();
