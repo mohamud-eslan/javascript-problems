3c00c3e4d804f0c3a50de1925a49ebc42c87a91c
mohamud <mohmud141@gmail.com>
2022-12-12 14:38:27 +0300

    an other solution of power of two

--- isPowerOfTwo2.js
+++ isPowerOfTwo2.js
@@ -1,15 +1,24 @@
-function isPowerOfTwo2(n){
+// function isPowerOfTwo2(n){
+//     if(n < 1){
+//         return false;
+//     }
+//     while(n > 1){
+//         if(n % 2 !== 0){
+//             return false;
+//         }
+//         n = n / 2;
+//     }
+//     return true;
+// }
+// console.log(isPowerOfTwo2(1));
+// console.log(isPowerOfTwo2(2));
+// console.log(isPowerOfTwo2(5));
+function isPowerOfBitwise(n){
     if(n < 1){
         return false;
     }
-    while(n > 1){
-        if(n % 2 !== 0){
-            return false;
-        }
-        n = n / 2;
-    }
-    return true;
+    return (n & (n - 1)) === 0;
 }
-console.log(isPowerOfTwo2(1));
-console.log(isPowerOfTwo2(2));
-console.log(isPowerOfTwo2(5));
+console.log(isPowerOfBitwise(1));
+console.log(isPowerOfBitwise(2));
+console.log(isPowerOfBitwise(5));
