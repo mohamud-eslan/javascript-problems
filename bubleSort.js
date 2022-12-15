12de9e9e2bbd0ecb14ac257df8725f32bc0be53d
mohamud <mohmud141@gmail.com>
2022-12-15 08:28:49 +0300

    buble sort

---
+++ bubleSort.js
@@ -0,0 +1,19 @@
+
+
+function bubleSort(arr){
+    let swapped;
+    do{
+        for(let i = 0; i < arr.length-1; i++){
+            if(arr[i] > arr[i + 1 ]){
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while(swapped);
+    }
+    const arr = [8, 20, -2, 4, -6];
+    bubleSort(arr)
+
+    console.log(arr);
