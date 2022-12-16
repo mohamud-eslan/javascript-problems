410c855c1be5b19c90288f9f71be3b4006f708e5
mohamud <mohmud141@gmail.com>
2022-12-16 19:19:39 +0300

    quick sort

--- bubleSort.js
+++ bubleSort.js
@@ -1,19 +1,22 @@
 
+    const arr = [8, 20, -2, 4, -6];
 
 function bubleSort(arr){
-    let swapped;
+    let swapped
     do{
+        swapped = false
         for(let i = 0; i < arr.length-1; i++){
             if(arr[i] > arr[i + 1 ]){
-                let temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                swapped = true;
+                let temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                swapped = true
+
             }
         }
-    } while(swapped);
+        
+    } while(swapped)
     }
-    const arr = [8, 20, -2, 4, -6];
+ 
     bubleSort(arr)
-
     console.log(arr);
